df19550362c66f3e501eda4c8615f76544290d45
Amira Rashad <amira.rashad1996@hotmail.com>
2020-12-30 11:09:01 +0500

    initial

---
+++ DB/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+var config = require("config");
+// config.get(db);
+// const URI="mongodb+srv://DBuser:amira123@cluster0.gdban.mongodb.net/layers?retryWrites=true&w=majority";
+
+
+const connectDB = async ()=>{
+    await mongoose.connect(config.get("db"),{ useUnifiedTopology: true, useNewUrlParser: true  });
+    console.log("Connection Established");
+}
+
+module.exports=connectDB;
